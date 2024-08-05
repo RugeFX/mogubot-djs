17304a16a416da24fc94bce04c318618b11170be
rugefx <zackfxg@gmail.com>
2024-08-05 21:48:30 +0700

    feat: revamp

--- src/commands/audio/play.ts
+++ src/commands/audio/play.ts
@@ -52,92 +52,11 @@ export default {
 
 		const voiceConnection = await connectToChannel(voiceChannel);
 
-		const queueExists = client.musicQueues.has(interaction.guildId);
-		if (!queueExists) {
-			const [audioPlayer] = playAudio(music, voiceConnection);
-
-			const musicQueue: MusicQueue = {
-				audioPlayer,
-				voiceConnection,
-				audios: [],
-			};
-
-			audioPlayer.on(AudioPlayerStatus.Idle, async () => {
-				console.log("we're idle");
-
-				const next = musicQueue.audios.shift();
-				if (!next) {
-					// TODO: idk if this is right
-					try {
-						await entersState(audioPlayer, AudioPlayerStatus.Playing, 10_000);
-
-						const adasdas = musicQueue.audios.shift()!;
-						const [, resource] = playAudio(adasdas, voiceConnection);
-						musicQueue.audioPlayer.play(resource);
-					}
-					catch (error) {
-						await interaction.reply({
-							content: "Failed to play next song",
-							ephemeral: true,
-						});
-						return;
-					}
-				}
-			});
-
-			client.musicQueues.set(interaction.guildId, musicQueue);
-
-			await interaction.reply({
-				content: `Joined voice channel ${voiceChannel.name} & Playing ${music.metadata.title}`,
-			});
-
-			return;
-		}
-
-		const musicQueue = client.musicQueues.get(interaction.guildId)!;
-
-		if (musicQueue.audioPlayer.state.status === AudioPlayerStatus.Idle) {
-			const [audioPlayer] = playAudio(music, voiceConnection);
-
-			musicQueue.audioPlayer = audioPlayer;
-
-			audioPlayer.on(AudioPlayerStatus.Idle, () => {
-				console.log("we're idle");
-
-				const next = musicQueue.audios.shift();
-				if (!next) {
-					setTimeout(() => {
-						if (!musicQueue.audios.length) {
-							voiceConnection.destroy();
-							client.musicQueues.delete(interaction.guildId);
-						}
-					}, 30_000);
-					return;
-				}
-
-				const [nextAudioPlayer] = playAudio(next, voiceConnection);
-				musicQueue.audioPlayer = nextAudioPlayer;
-			});
-		}
-		else {
-			musicQueue.audios.push(music);
-		}
-
 		await interaction.reply({
 			content: `Playing / Queued ${music.metadata.title}`,
 		});
 	},
 } as Command;
-
-function playAudio(music: Music, voiceConnection: VoiceConnection) {
-	const audioPlayer = createAudioPlayer();
-	const resource = createAudioResource(music.source, { metadata: music.metadata });
-
-	voiceConnection.subscribe(audioPlayer);
-	audioPlayer.play(resource);
-
-	return [audioPlayer, resource] as const;
-}
 
 async function connectToChannel(channel: VoiceBasedChannel) {
 	const connection = joinVoiceChannel({
